fe1c338fda652e9906a98340afb3e81f0dc024c4
TAJIMA, Keiichi <k1tajima@mi.to>
2019-04-06 20:42:06 +0900

    小修正

--- run.ps1
+++ run.ps1
@@ -3,16 +3,16 @@ Param(
 )
 
 # Run setup_nginx_ssl.ps1 simply.
-& (Join-Path $PSScriptRoot "script\setup_nginx_ssl.ps1") `
-    -CommonName www.example.com `
+& (Join-Path $PSScriptRoot 'script\setup_nginx_ssl.ps1') `
+    -CommonName 'www.example.com' `
     -Email 'you@example.com' `
     -Cert:$Cert
 
-# Run setup_nginx_ssl.ps1 with all optins.
-# & (Join-Path $PSScriptRoot "script\setup_nginx_ssl.ps1") `
-# -CommonName www.example.com `
-# -AlternativeNames 'proxy.example.com,app.example.jp'
-# -Email 'you@example.com' `
-# -NginxRootPath 'C:\nginx' `
-# -CertStorePath 'C:\SSL\cert\win-acme' `
-# -Cert:$Cert
+# Run setup_nginx_ssl.ps1 with alsl optins.
+# & (Join-Path $PSScriptRoot 'script\setup_nginx_ssl.ps1') `
+#     -CommonName 'www.example.com' `
+#     -AlternativeNames 'proxy.example.com,app.example.jp'
+#     -Email 'you@example.com' `
+#     -NginxRootPath 'C:\nginx' `
+#     -CertStorePath 'C:\SSL\cert\win-acme' `
+#     -Cert:$Cert
