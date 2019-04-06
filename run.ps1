4a43e51f88ba47343ec7c98d4468e0355d34017d
TAJIMA, Keiichi <k1tajima@mi.to>
2019-04-06 18:54:58 +0900

    run.ps1, describe all options.

--- run.ps1
+++ run.ps1
@@ -2,7 +2,17 @@ Param(
     [switch] $Cert
 )
 
+# Run setup_nginx_ssl.ps1 simply.
 & (Join-Path $PSScriptRoot "script\setup_nginx_ssl.ps1") `
     -CommonName www.example.com `
     -Email 'you@example.com' `
     -Cert:$Cert
+
+# Run setup_nginx_ssl.ps1 with all optins.
+# & (Join-Path $PSScriptRoot "script\setup_nginx_ssl.ps1") `
+# -CommonName www.example.com `
+# -AlternativeNames 'proxy.example.com,app.example.jp'
+# -Email 'you@example.com' `
+# -NginxRootPath 'C:\nginx' `
+# -CertStorePath 'C:\SSL\cert\win-acme' `
+# -Cert:$Cert
